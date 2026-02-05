42cd330cef70951449a521dd5d0c263e4c5a9e89
Maike Sperk <maike.sperk@folkhalsomyndigheten.se>
2026-02-05 16:10:26 +0100

    add sites

---
+++ app_V5.R
@@ -0,0 +1,122 @@
+# load packages ----
+pkgs <- c("shiny", "plotly", "ggplot2", "dplyr")
+install.packages(setdiff(pkgs, rownames(installed.packages())))
+invisible(lapply(pkgs, FUN = library, character.only = TRUE))
+
+# load data ----
+# load nation data
+df_nation <- read.csv2("./data/Belgium_export-nation.csv", sep = ";")
+
+# load the data by site and select appropriate variables
+df_site <- read.csv2("./data/Belgium_export-site.csv", sep = ";") %>%
+  select(siteName, date, value, value_load, value_pmmv, value_avg14d_past, value_load_avg14d_past, value_pmmv_avg14d_past)
+
+# bind nation and site data
+df <- rbind(df_nation, df_site)
+
+# clean data
+df$date <- as.Date(df$date)
+df$value_pmmv <- as.numeric(df$value_pmmv)*1000
+df$value_pmmv_avg14d_past <- as.numeric(df$value_pmmv_avg14d_past)*1000
+
+# ui ----
+ui <- navbarPage(
+  title = "Wastewater surveillance",
+  
+  tabPanel(
+    "About",
+    # Custom CSS for green header bar
+    tags$style(HTML("
+    .top-bar {
+      background-color: #e8f5e9;
+      padding: 15px;
+      margin-bottom: 20px;
+      border-radius: 5px;
+      color: white;
+    }
+    .top-bar label {
+      color: white;
+      font-weight: bold;
+    }
+    .info-box {
+      background-color: #4CAF50;
+      padding: 15px;
+      border-radius: 5px;
+      text-align: center;
+      font-weight: bold;
+      border: 1px solid #c8e6c9;
+      margin-bottom: 20px;
+    }
+  ")),
+    
+    # Bottom acknowledgment box
+    div(class = "info-box",
+        "Welcome to this page describing national wastewater data on viral load of SARS-CoV-2" )
+  ),
+  
+  
+  tabPanel(
+    "SARS-CoV-2",
+    # Bottom acknowledgment box
+    div(class = "info-box",
+        "Add a description of the surveillance..." )
+    ,
+    
+    # Three horizontal info boxes
+    fluidRow(
+      column(4, div(class = "info-box", "Number of site = 30", style = "color: purple;")),
+      column(4, div(class = "info-box", "Next sampling date = Wednesay", style = "color: brown;")),
+      column(4, div(class = "info-box", "Next dashboard update = Monday", style = "color: white;")
+      )
+    ),
+    
+    # Top bar with dropdown
+    div(class = "top-bar",
+        style = "color: black;",
+        selectInput(
+          inputId = "site",
+          label = "Select sampling site",
+          choices = unique(df$siteName),
+          selected = "Belgium"
+        )
+    ),
+    
+    # Main content
+    plotlyOutput("viralPlot")
+    ,
+    
+    # Bottom acknowledgment box
+    div(class = "info-box",
+        "Acknowledgment: Thanks to all the sites providing us with wastewater samples" )
+    
+  )
+  
+  
+)
+
+# server ----
+server <- function(input, output, session) {
+  
+  filtered_data <- reactive({
+    df %>% filter(siteName == input$site)
+  })
+  
+  output$viralPlot <- renderPlotly({
+    
+    data <- filtered_data()
+    
+    p <- ggplot(data, aes(x = date)) +
+      geom_point(aes(y = value_pmmv), colour = "#92D050", size = 1, na.rm = T) +
+      scale_y_continuous(limits = c(0, NA)) +
+      labs(
+        title = paste(input$site),
+        x = "", y = "(c/c)"
+      ) +
+      theme_minimal(base_size = 14)
+    
+    ggplotly(p)
+  })
+}
+
+# shinyApp ----
+shinyApp(ui, server)
